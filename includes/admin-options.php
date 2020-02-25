b92465fff62e2767d32d54c1b57c001708ecf964
Michał <mwargan@gmail.com>
2020-02-25 13:02:14 +0100

    Add files via upload

---
+++ includes/admin-options.php
@@ -0,0 +1,123 @@
+<?php
+
+defined('ABSPATH') or die('No script kiddies please!');
+
+/**
+ * Class WPOSSO_Admin
+ *
+ * @author Justin Greer <justin@justin-greer.com
+ * @package WP Single Sign On Client
+ */
+class WPOSSO_Admin
+{
+
+    protected $option_name = 'wposso_options';
+
+    public static function init()
+    {
+        add_action('admin_init', array(new self, 'admin_init'));
+        add_action('admin_menu', array(new self, 'add_page'), 11);
+
+    }
+
+    /**
+     * [admin_init description]
+     * @return [type] [description]
+     */
+    public function admin_init()
+    {
+        register_setting('wposso_options', $this->option_name, array($this, 'validate'));
+    }
+
+    /**
+     * [add_page description]
+     */
+    public function add_page()
+    {
+//         add_options_page( 'Log in with M Media', 'Log in with M Media', 'manage_options', 'wposso_settings', array(
+        //             $this,
+        //             'options_do_page'
+        //         ) );
+        add_submenu_page('mmedia_main_menu', 'Log in with M Media',
+            'Log in with M Media', 'manage_options', 'm_media_login', array(
+                $this,
+                'options_do_page',
+            ));
+
+    }
+    /**
+     * [options_do_page description]
+     * @return [type] [description]
+     */
+    public function options_do_page()
+    {
+        $options = get_option($this->option_name);
+        ?>
+
+        <div class="wrap">
+			<div class="align-center-mmedia" style="text-align: center;padding-top:15px;">
+        <img src="https://mmediagroup.fr/images/logo.png" height="75">
+        <p style="font-weight: 500;">We make websites and handle your marketing.</p>
+    </div>
+    <div class="card">
+        <h3>Step 1</h3>
+        <p>Obtain a Client ID and Secret from M Media.</p>
+        <p>Your redirect URL is <strong><?php echo site_url('?auth=sso'); ?></strong></p>
+    </div>
+    <div class="card">
+        <h3>Step 2</h3>
+        <p>Input your Client ID and Secret.</p>    <form method="post" action="options.php">
+                        <?php settings_fields('wposso_options');?>
+                        <table class="form-table">
+                            <tr valign="top">
+                                <th scope="row">Client ID</th>
+                                <td>
+                                    <input type="text" name="<?php echo $this->option_name ?>[client_id]" min="10"
+                                           value="<?php echo $options["client_id"]; ?>"/>
+                                </td>
+                            </tr>
+
+                            <tr valign="top">
+                                <th scope="row">Client Secret</th>
+                                <td>
+                                    <input type="text" name="<?php echo $this->option_name ?>[client_secret]" min="10"
+                                           value="<?php echo $options["client_secret"]; ?>"/>
+                                </td>
+                            </tr>
+
+                           <!--  <tr valign="top">
+                                <th scope="row">Redirect to the dashboard after signing in</th>
+                                <td>
+                                    <input type="checkbox"
+                                           name="<?php echo $this->option_name ?>[redirect_to_dashboard]"
+                                           value="1" <?php echo $options['redirect_to_dashboard'] == 1 ? 'checked="checked"' : ''; ?> />
+                                </td>
+                            </tr> -->
+                        </table>
+
+                        <p class="submit">
+                            <input type="submit" class="button button-mmedia" value="<?php _e('Save Changes')?>"/>
+                        </p>
+
+                </form>
+    </div>
+        </div>
+		<?php
+}
+
+    /**
+     * Settings Validation
+     *
+     * @param  [type] $input [description]
+     *
+     * @return [type]        [description]
+     */
+    public function validate($input)
+    {
+        $input['redirect_to_dashboard'] = isset($input['redirect_to_dashboard']) ? $input['redirect_to_dashboard'] : 0;
+
+        return $input;
+    }
+}
+
+WPOSSO_Admin::init();
---
+++ mmedia_login.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Plugin Name: Log in with M Media
+ * Plugin URI: https://mmediagroup.fr
+ * Version: 1.0.0
+ * Description: Provides Simple Single Sign On integration with M Media. Based off of the original work by http://dash10.digital
+ * Author: M Media
+ * Author URI: https://mmediagroup.fr
+ * License: GPL2
+ *
+ * This program is GLP but; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of.
+ */
+
+defined('ABSPATH') or die('No script kiddies please!');
+
+if (!class_exists('Smashing_Updater')) {
+    include_once plugin_dir_path(__FILE__) . 'updater.php';
+}
+$updater = new Smashing_Updater(__FILE__);
+$updater->set_username('M-Media-Group');
+$updater->set_repository('mmedia-login-wp-plugin');
+/*
+$updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
+ */
+$updater->initialize();
+
+if (!defined('WPOSSO_FILE')) {
+    define('WPOSSO_FILE', plugin_dir_path(__FILE__));
+}
+
+// Require the main plugin clas
+require_once WPOSSO_FILE . '/library/class-wposso-client.php';
+
+add_action('admin_menu', array(new WPOSSO_Client, 'plugin_init'));
+register_activation_hook(__FILE__, array(new WPOSSO_Client, 'setup'));
+register_activation_hook(__FILE__, array(new WPOSSO_Client, 'upgrade'));
