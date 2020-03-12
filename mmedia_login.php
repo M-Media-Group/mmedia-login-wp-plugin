8374e9a94d88a30073567bcf40761b480b9129aa
Michał <mwargan@gmail.com>
2020-03-12 19:29:13 +0100

    Update mmedia_login.php

--- mmedia_login.php
+++ mmedia_login.php
@@ -18,21 +18,6 @@
  */
 defined('ABSPATH') or die('No script kiddies please!');
 
-if (!class_exists('Smashing_Updater')) {
-    include_once plugins_url('mmedia/updater.php');
-}
-$updater = new Smashing_Updater(__FILE__);
-$updater->set_username('M-Media-Group');
-$updater->set_repository('mmedia-login-wp-plugin');
-/*
-$updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
- */
-$updater->initialize();
-
-if (!defined('WPOSSO_FILE')) {
-    define('WPOSSO_FILE', plugin_dir_path(__FILE__));
-}
-
 // Require the main plugin clas
 require_once WPOSSO_FILE.'/library/class-wposso-client.php';
 
